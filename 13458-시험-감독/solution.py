e291f01fb4a03532af9f574798d41ecbf70beb7a
Jang <hjjang501@gmail.com>
2025-05-24 17:33:17 +0900

    Add solution code 5/24/25

---
+++ 13458-시험-감독/solution.py
@@ -0,0 +1,18 @@
+from math import ceil
+import sys
+
+def solution(rooms, sup, sub_sup):
+    cnt = 0
+
+    for examinees in rooms:
+        examinees = max(examinees - sup, 0)
+        cnt += 1
+        cnt += ceil(examinees / sub_sup)
+    
+    return cnt
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline().strip())
+    rooms = [int(val) for val in sys.stdin.readline().strip().split(" ")]
+    sup, sub_sup = map(int, sys.stdin.readline().strip().split(" "))
+    print(solution(rooms, sup, sub_sup))
---
+++ 14501-퇴사/solution.py
@@ -0,0 +1,23 @@
+import sys
+
+def solution(tasks):
+    max_days = len(tasks)
+    pay = [0 for _ in range(max_days + 2)]
+
+    for d in reversed(range(1, max_days + 1)):
+        days, rewards = tasks[d - 1]
+        
+        if (d + days) > (max_days + 1):
+            continue
+
+        for i in range(d + days, max_days + 2):
+            pay[d] = max(pay[d], pay[i] + rewards)
+        for i in range(d+1, d + days):
+            pay[d] = max(pay[d], pay[i])
+        
+    return max(pay)
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline().strip())
+    tasks = [tuple(map(int, sys.stdin.readline().strip().split(" "))) for _ in range(n)]
+    print(solution(tasks))
